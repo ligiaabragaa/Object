38e0c1c6d93d5ba7bc49374bda749b2e0d01b8c4
ligiaabragaa <119192780+ligiaabragaa@users.noreply.github.com>
2022-12-10 11:35:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let x = 17;
-let y = 21;
-let z = 42;
-
-console.log(x<y, y<z);
-console.log(x != z, y>z);
-console.log(z == y*2);
-console.log(x+y>z);
-console.log(x**2 + y**2 > z**2);
+let objDevInHouse= {
+    plataforma: "DevInHouse",
+    cursos: {
+      dados: ["SQL"],
+      backend: ["Java", "Spring"],
+      frontend: ["Javascript", "React"]
+  },
+     objUser : {
+      nome: ["Ligia"],
+      idade:[38],
+      cidade: ["Florianopolis"]
+  }
+  };
+  objDevInHouse.objUser.nome[0]= "Ligia Daniela Melo Braga";
+  objDevInHouse.objUser.idade[0]= "38";
+  console.table(objDevInHouse)
